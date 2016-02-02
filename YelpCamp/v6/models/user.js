9b7fd8c095d11f2e44cc67889a2ddd35a713637f
DanBonehill <DanBonehill@me.com>
2016-02-02 23:31:09 +0000

    Added passportLocalMongoose plugin to User

--- YelpCamp/v6/models/user.js
+++ YelpCamp/v6/models/user.js
@@ -1,8 +1,10 @@
-var mongoose = require("mongoose");
+var mongoose        = require("mongoose"),
+    passportLocalMongoose   = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
     username: String,
     password: String
 });
 
+UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User", UserSchema);
