89d58eb089638ae80315ac5bb1a4ffe15c90f4c0
DanBonehill <DanBonehill@me.com>
2016-02-02 18:40:23 +0000

    Added Authentication Demo. Added packages and basic routes

---
+++ Authentication/AuthDemo/app.js
@@ -0,0 +1,18 @@
+var express     = require("express"),
+    mongoose    = require("mongoose"),
+    app         = express();
+    
+mongoose.connect("mongodb://localhost/autho_demo_app");
+    
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("home");
+});
+app.get("/secret", function(req, res) {
+   res.render("secret"); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+   console.log("Server Started"); 
+});
