254dccd9a5e719161f0da54698d6439243488449
DanBonehill <DanBonehill@me.com>
2016-02-03 22:43:59 +0000

    Added Middleware to pass user data between routes

--- YelpCamp/v6/app.js
+++ YelpCamp/v6/app.js
@@ -30,6 +30,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next) {
+   res.locals.currentUser = req.user;
+   next();
+});
+
 // Define routes
 // Landing page route
 app.get("/", function(req, res) {
