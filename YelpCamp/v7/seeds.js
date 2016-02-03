8d8f4ec032080b20f20f97d4123def84846d05cf
DanBonehill <DanBonehill@me.com>
2016-02-03 22:46:55 +0000

    Created v7 - refactoring code

---
+++ YelpCamp/v7/seeds.js
@@ -0,0 +1,57 @@
+var mongoose    = require("mongoose");
+var Campground  = require("./models/campground");
+var Comment     = require("./models/comment");
+
+var data = [
+    {
+        name: "Clouds Rest",
+        image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        name: "Desert Farm",
+        image: "https://farm4.staticflickr.com/3859/15123592300_6eecab209b.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    },
+    {
+        name: "Canyon Floor",
+        image: "https://farm4.staticflickr.com/3859/15123592300_6eecab209b.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
+];
+
+function seedDB() {
+    // Remove all Campgrounds
+    Campground.remove({}, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Removed Campgrounds");
+            // Add Campgrounds
+            data.forEach(function(seed) {
+                Campground.create(seed, function(err, campground) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Added a Campground");
+                        // Create a Comment
+                        Comment.create(
+                            {
+                                text: "This place is great",
+                                author: "Dave"
+                            }, function(err, comment) {
+                                if (err) {
+                                    console.log(err);
+                                } else {
+                                    campground.comments.push(comment);
+                                    campground.save();
+                                    console.log("Created new comment");
+                                }
+                            });
+                    }
+                });
+            });
+        }
+    });
+}
+module.exports = seedDB;
