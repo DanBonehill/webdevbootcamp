2acb0c3af8bda4dff820f5a635624f1c908cdd5f
DanBonehill <DanBonehill@me.com>
2016-02-04 18:24:43 +0000

    Added Association for Campgrounds

--- YelpCamp/v9/routes/campgrounds.js
+++ YelpCamp/v9/routes/campgrounds.js
@@ -19,18 +19,24 @@ router.post("/", isLoggedIn, function(req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
     var newCampground = {
         name: name,
         image: image,
-        description: desc
+        description: desc,
+        author: author
     };
     // Create a new campground and save to DB
     Campground.create(newCampground, function(err, newlyCreated) {
         if (err) {
             console.log(err);
         } else {
-          // redirect back to campgrounds page
-        res.redirect("/campgrounds");  
+            // redirect back to campgrounds page
+            console.log(newlyCreated);
+            res.redirect("/campgrounds");  
         }
     });
 });
