a77f2da1cf28b38fdd02fe7fb6fdfd45f18e21c4
DanBonehill <DanBonehill@me.com>
2016-02-05 11:39:41 +0000

    Added Connect-Flash

--- YelpCamp/v11/app.js
+++ YelpCamp/v11/app.js
@@ -2,6 +2,7 @@ var express         = require("express"),
     app             = express(),
     bodyParser      = require("body-parser"),
     mongoose        = require("mongoose"),
+    flash           = require("connect-flash"),
     passport        = require("passport"),
     LocalStrategy   = require("passport-local"),
     methodOverride  = require("method-override"),
@@ -23,6 +24,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 
 // Passport Configuration
 app.use(require("express-session")({
