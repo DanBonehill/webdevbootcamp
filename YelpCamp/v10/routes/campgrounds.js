6de834a8d4cdc92ac55da9ee86c5cb61b78012ea
DanBonehill <DanBonehill@me.com>
2016-02-05 00:42:08 +0000

    Added Authorization for users to Edit or Delete their own Campgrounds

--- YelpCamp/v10/routes/campgrounds.js
+++ YelpCamp/v10/routes/campgrounds.js
@@ -58,17 +58,13 @@ router.get("/:id", function(req, res) {
     });
 });
 // Edit Campground Route
-router.get("/:id/edit", function(req, res) {
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
     Campground.findById(req.params.id, function(err, foundCampground) {
-       if (err) {
-           res.redirect("/campgrounds");
-       } else {
-           res.render("campgrounds/edit", {campground: foundCampground});
-       }
+        res.render("campgrounds/edit", {campground: foundCampground});
     });
 });
 // Update Campground Route
-router.put("/:id", function(req, res) {
+router.put("/:id", checkCampgroundOwnership, function(req, res) {
    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
        if (err) {
            res.redirect("/campgrounds");
@@ -78,7 +74,7 @@ router.put("/:id", function(req, res) {
    });
 });
 // Destroy Route
-router.delete("/:id", function(req, res) {
+router.delete("/:id", checkCampgroundOwnership, function(req, res) {
     Campground.findByIdAndRemove(req.params.id, function(err) {
        if (err) {
            res.redirect("/campgrounds");
@@ -93,5 +89,22 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+}
+function checkCampgroundOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Campground.findById(req.params.id, function(err, foundCampground) {
+            if (err) {
+                res.redirect("/campgrounds");
+            } else {
+                if (foundCampground.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 module.exports = router;
