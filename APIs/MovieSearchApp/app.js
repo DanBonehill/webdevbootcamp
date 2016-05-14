91e557c7e9f83a8c0eca8bd4b100ced9c877fe7e
DanBonehill <DanBonehill@me.com>
2016-05-14 11:12:16 +0000

    Add results page to display results

--- APIs/MovieSearchApp/app.js
+++ APIs/MovieSearchApp/app.js
@@ -2,11 +2,13 @@ var express = require("express"),
     request = require("request"),
     app     = express();
     
+app.set("view engine", "ejs");
+    
 app.get("/results", function(req, res) {
-    request("http://omdbapi.com/?s=Frozen", function(err, response, body) {
+    request("http://omdbapi.com/?s=Star", function(err, response, body) {
         if (!err && response.statusCode === 200) {
             var data = JSON.parse(body);
-            res.send(data);
+            res.render("results", {data: data});
         }
     });
 });
